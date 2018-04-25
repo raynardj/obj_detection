7e059c9279e628057c1761732b28894e28c303b2
zhangxiaochen <zhangxiaochen@zenmen.com>
2018-04-25 15:05:56 +0800

    one anchor box, one seen

--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 
 COLORS = ["#ff0000","#ffff00","#ff00ff","#00ffff","#00ff00","#0000ff","#ffffff"]
+bx_grid=torch.arange(0,BOX).unsqueeze(0).unsqueeze(0).repeat(1,FEAT_W,FEAT_H,1)
 
 def plot_bb(img,bbdf):
     fig,ax = plt.subplots(1)
@@ -45,26 +46,23 @@ def data_to_df(y_pred,head=10):
     return pd.concat([df_lbl,df_bbox],axis=1).sort_values(by="conf",ascending=False).head(head).reset_index()
 
 def data_to_df_bmark(y_pred,head=5,bm=.50):
-    y_pred=y_pred.detach()
-    conf_, idx = torch.max(y_pred[..., 4], dim=-1)
-    conf_b = (conf_>bm)
-    if conf_b.sum().data[0]==0:
-        return None
-    else:
-        conf = y_pred[...,4][conf_b]
+    bs = y_pred.size()[0]
+    val, idx = torch.max(y_pred[..., 4], dim=-1)
+    pick = (idx.unsqueeze(-1) == bx_grid.repeat(bs, 1, 1, 1).long())
 
-        conf_cls = y_pred[...,5:][conf_b].view(-1,CLS_LEN)
+    cls_val, cls_idx = torch.max(y_pred[..., 5:], dim=-1)
+    cls_ = cls_idx[pick].view(-1, 1).float()
 
-        val_max, idx_max = torch.max(conf_cls, dim=-1)
-    
-        df_lbl = pd.DataFrame({"conf":conf.data.numpy(),"cate":idx_max.data.numpy()})
+    conf_ = y_pred[..., 4][pick].view(-1, 1)
+    box_ = y_pred[..., :4][pick.unsqueeze(-1).repeat(1, 1, 1, 1, 4)].view(-1, 4) * 32
 
-        bbmax = y_pred[...,:4][conf_b].contiguous().view(-1,4).data.numpy()*32
-        df_bbox = pd.DataFrame(bbmax,columns=["x","y","w","h"])
-    
-        df_bbox["x"]=df_bbox["x"]-(df_bbox["w"]/2)
-        df_bbox["y"]=df_bbox["y"]-(df_bbox["h"]/2)
-        df_bbox["x"][df_bbox["x"]<0]=0.
-        df_bbox["y"][df_bbox["y"]<0]=0.
-    
-        return pd.concat([df_lbl,df_bbox],axis=1) # .sort_values(by="conf",ascending=False).head(head).reset_index()
+    combine = torch.cat([box_, conf_, cls_], dim=-1).numpy()
+
+    df_bbox = pd.DataFrame(combine[combine[..., 4] > bm], columns=["x", "y", "w", "h", "conf", "cate"])
+
+    df_bbox["x"] = df_bbox["x"] - (df_bbox["w"] / 2)
+    df_bbox["y"] = df_bbox["y"] - (df_bbox["h"] / 2)
+    df_bbox["x"][df_bbox["x"] < 0] = 0.
+    df_bbox["y"][df_bbox["y"] < 0] = 0.
+
+    return df_bbox
